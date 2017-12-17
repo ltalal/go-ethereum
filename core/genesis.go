cd4526bc76682cec85b17e22b6be2c9fd42097b6
Leonid Talalaev <leonid.talalaev@gmail.com>
2017-12-17 15:55:47 +0200

    Gas limit increased

--- core/genesis.go
+++ core/genesis.go
@@ -353,7 +353,7 @@ func DeveloperGenesisBlock(period uint64, faucet common.Address) *Genesis {
 	return &Genesis{
 		Config:     &config,
 		ExtraData:  append(append(make([]byte, 32), faucet[:]...), make([]byte, 65)...),
-		GasLimit:   6283185,
+		GasLimit:   16777216,
 		Difficulty: big.NewInt(1),
 		Alloc: map[common.Address]GenesisAccount{
 			common.BytesToAddress([]byte{1}): {Balance: big.NewInt(1)}, // ECRecover
